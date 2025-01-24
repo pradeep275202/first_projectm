68fc9c07d399cf569c121014b5c71d8317fca3b9
pradeep <pr275202@gmail.com>
2025-01-24 22:27:42 +0530

    24

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,12 +1,41 @@
 function showinputValuse() {
-  var input = document.getElementById("input");
-  var inputValuse = input.value;
-  console.log(inputValuse)
+  var input = document.getElementById("input").value;
+  // let storeValuse = localStorage.getItem("userInput")
+  let a=localStorage.setItem("name", input)
+  // let stredValuse=localStorage.getItem("input")
+
+  console.log(a)
+
+  // var inputValuse = input.value;
+  // console.log(input)
   
-  var table = document.createElement("table")
-  table.style.border ='1'
-  var tr = document.createElement("tr")
-  tr.innerText ="inputValuse"
-  tr.appendChild(table)
+  let newRow =document.createElement('tr')
+  let newCell =document.createElement('td')
+ 
+  newCell.textContent=  input;
+  newCell.style.border='1px solid black'
+  newCell.style.width='100%'
+  newRow.appendChild(newCell);
+  
+  let select=document.getElementById("Table_container")
+  let button =document.createElement('button')
+  button.innerText="Delete"
+  button.style.float='right'
+  newCell.append(button)
+  newCell.remove()
+
+  document.getElementById("Table_container").appendChild(newRow)
 
 }
+
+// const table = document.querySelector('.Table_container');
+
+// table.addEventListener('click', (event) => {
+
+//   if (event.target.newCell.contains('delete-button')) {
+
+//     deleteCell(event); 
+
+//   }
+
+// });
